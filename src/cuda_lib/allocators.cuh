eaa1d8b5d44ac7adec9bec1977d03f6c64b32cd9
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-01-22 01:54:08 +0100

    Implemented mem trasnfer

--- src/cuda_lib/allocators.cuh
+++ src/cuda_lib/allocators.cuh
@@ -2,11 +2,38 @@
 #define ALLOCATORS_CUH
 
 #include <data.cuh>
+#include <atomic>
+#include <global_conf.cuh>
+#include <cuda_runtime.h>
 
 static constexpr uint64_t kKb = 1024;
 static constexpr uint64_t kMb = 1024 * kKb;
 static constexpr size_t kCpuThreadChunkSize = 16 * kMb;
 static constexpr size_t kGpuThreadChunkSize = 8 * kKb;
+
+static constexpr uint32_t kSpinLockFree = 0;
+static constexpr uint32_t kSpinLockLocked = 1;
+
+class CpuSpinLock {
+public:
+    explicit CpuSpinLock(uint32_t &lock_base) : _lock_base(reinterpret_cast<std::atomic<uint32_t> *>(&lock_base)) {
+    }
+
+    void lock() {
+        uint32_t expected = kSpinLockFree;
+
+        while (!_lock_base->compare_exchange_strong(expected, kSpinLockLocked, std::memory_order_acquire)) {
+            expected = kSpinLockFree;
+        }
+    }
+
+    void unlock() {
+        _lock_base->store(kSpinLockFree, std::memory_order_release);
+    }
+
+protected:
+    std::atomic<uint32_t> *_lock_base;
+};
 
 class FastAllocator {
     // ------------------------------
@@ -31,6 +58,7 @@ public:
         /* verify provided data */
         assert(num_threads * thread_chunk_size < kPageSize);
         assert(thread_chunk_size % sizeof(Node_) == 0);
+        assert(kPageSizeInTypeSize % _thread_chunk_size_in_type == 0);
 
         /* allocate memory for management data */
         _pages = new Node_ *[kMaxPages]{};
@@ -42,12 +70,12 @@ public:
 
         /* set thread bottoms */
         for (size_t thread_idx = 0; thread_idx < num_threads; ++thread_idx) {
-            _thread_bottoms[thread_idx] = thread_idx * thread_chunk_size;
+            _thread_bottoms[thread_idx] = thread_idx * _thread_chunk_size_in_type;
         }
 
         /* prepare allocator data */
         _last_page = 0;
-        _last_page_offset = num_threads * thread_chunk_size;
+        _last_page_offset = num_threads * _thread_chunk_size_in_type;
     }
 
     // ------------------------------
@@ -64,11 +92,22 @@ public:
 
     template<bool isGpu = false>
     [[nodiscard]] FAST_CALL_ALWAYS uint32_t AllocateNode(const uint32_t t_idx) {
-        if constexpr (isGpu) {
-            return _allocateNodeGpu(t_idx);
-        } else {
-            return _allocateNodeCpu(t_idx);
+        /* get current thread top */
+        uint32_t top = _thread_tops[t_idx]++;
+
+        /* check if we need new block - lazy allocation */
+        if (top > _thread_chunk_size_in_type) {
+            /* get new block */
+            if constexpr (isGpu) {
+            } else {
+                _thread_bottoms[t_idx] = _AllocateNewBlockCpu();
+            }
+
+            _thread_tops[t_idx] = 0;
+            top = 0;
         }
+
+        return _thread_bottoms[t_idx] + top;
     }
 
     // ------------------------------
@@ -90,22 +129,132 @@ public:
     }
 
     [[nodiscard]] FastAllocator *DumpToGPU() const {
-        return nullptr;
+        /* allocate the object itself */
+        FastAllocator *d_allocator;
+        CUDA_ASSERT_SUCCESS(cudaMallocAsync(&d_allocator, sizeof(FastAllocator), g_cudaGlobalConf->asyncStream));
+        CUDA_ASSERT_SUCCESS(cudaMemcpyAsync(d_allocator, this, sizeof(FastAllocator), cudaMemcpyHostToDevice,
+            g_cudaGlobalConf->asyncStream));
+
+        /* copy thread tops */
+        uint32_t *d_thread_tops;
+        CUDA_ASSERT_SUCCESS(
+            cudaMallocAsync(&d_thread_tops, _num_threads * sizeof(uint32_t), g_cudaGlobalConf->asyncStream));
+        CUDA_ASSERT_SUCCESS(
+            cudaMemcpyAsync(d_thread_tops, _thread_tops, _num_threads * sizeof(uint32_t), cudaMemcpyHostToDevice,
+                g_cudaGlobalConf->asyncStream));
+
+        /* copy thread bottoms */
+        uint32_t *d_thread_bottoms;
+        CUDA_ASSERT_SUCCESS(
+            cudaMallocAsync(&d_thread_bottoms, _num_threads * sizeof(uint32_t), g_cudaGlobalConf->asyncStream));
+        CUDA_ASSERT_SUCCESS(
+            cudaMemcpyAsync(d_thread_bottoms, _thread_bottoms, _num_threads * sizeof(uint32_t), cudaMemcpyHostToDevice,
+                g_cudaGlobalConf->asyncStream));
+
+        /* allocate pages */
+        auto h_pages = new Node_ *[kMaxPages]{};
+        for (uint32_t page_idx = 0; page_idx < _last_page; ++page_idx) {
+            Node_ *d_page;
+            CUDA_ASSERT_SUCCESS(cudaMallocAsync(&d_page, kPageSize, g_cudaGlobalConf->asyncStream));
+            CUDA_ASSERT_SUCCESS(cudaMemcpyAsync(d_page, _pages[page_idx], kPageSize, cudaMemcpyHostToDevice,
+                g_cudaGlobalConf->asyncStream));
+
+            h_pages[page_idx] = d_page;
+        }
+
+        /* copy page table */
+        Node_ **d_pages;
+        CUDA_ASSERT_SUCCESS(cudaMallocAsync(&d_pages, kMaxPages * sizeof(Node_ *), g_cudaGlobalConf->asyncStream));
+        CUDA_ASSERT_SUCCESS(cudaMemcpyAsync(d_pages, h_pages, kMaxPages * sizeof(Node_ *), cudaMemcpyHostToDevice,
+            g_cudaGlobalConf->asyncStream));
+
+        /* update object pointers */
+        CUDA_ASSERT_SUCCESS(
+            cudaMemcpyAsync(&d_allocator->_thread_tops, &d_thread_tops, sizeof(uint32_t *), cudaMemcpyHostToDevice,
+                g_cudaGlobalConf->asyncStream));
+        CUDA_ASSERT_SUCCESS(
+            cudaMemcpyAsync(&d_allocator->_thread_bottoms, &d_thread_bottoms, sizeof(uint32_t *), cudaMemcpyHostToDevice
+                , g_cudaGlobalConf->asyncStream));
+        CUDA_ASSERT_SUCCESS(
+            cudaMemcpyAsync(&d_allocator->_pages, &d_pages, sizeof(Node_ **), cudaMemcpyHostToDevice, g_cudaGlobalConf->
+                asyncStream));
+
+        /* free temporary data */
+        delete[] h_pages;
+
+        /* final sync */
+        CUDA_ASSERT_SUCCESS(cudaStreamSynchronize(g_cudaGlobalConf->asyncStream));
+
+        return d_allocator;
     }
 
     static void DeallocGPU(FastAllocator *allocator) {
+        /* prepare host page table */
+        auto h_pages = new Node_ *[kMaxPages]{};
+
+        /* copy page table */
+        CUDA_ASSERT_SUCCESS(
+            cudaMemcpyAsync(h_pages, allocator->_pages, kMaxPages * sizeof(Node_ *), cudaMemcpyDeviceToHost,
+                g_cudaGlobalConf->asyncStream));
+
+        /* delete individual pages */
+        for (uint32_t page_idx = 0; page_idx < kMaxPages; ++page_idx) {
+            if (h_pages[page_idx] != nullptr) {
+                CUDA_ASSERT_SUCCESS(cudaFreeAsync(h_pages[page_idx], g_cudaGlobalConf->asyncStream));
+            }
+        }
+
+        /* free page table */
+        CUDA_ASSERT_SUCCESS(cudaFreeAsync(allocator->_pages, g_cudaGlobalConf->asyncStream));
+
+        /* free thread tops */
+        CUDA_ASSERT_SUCCESS(cudaFreeAsync(allocator->_thread_tops, g_cudaGlobalConf->asyncStream));
+
+        /* free thread bottoms */
+        CUDA_ASSERT_SUCCESS(cudaFreeAsync(allocator->_thread_bottoms, g_cudaGlobalConf->asyncStream));
+
+        /* free allocator object */
+        CUDA_ASSERT_SUCCESS(cudaFreeAsync(allocator, g_cudaGlobalConf->asyncStream));
+
+        /* cleanup */
+        delete[] h_pages;
+
+        /* final sync */
+        CUDA_ASSERT_SUCCESS(cudaStreamSynchronize(g_cudaGlobalConf->asyncStream));
     }
 
     // ------------------------------
     // protected methods
     // ------------------------------
 protected:
-    [[nodiscard]] FAST_DCALL_ALWAYS uint32_t _allocateNodeGpu(const uint32_t t_idx) {
-        return 0;
-    }
+    // ------------------------------
+    // Allocator calls
+    // ------------------------------
 
-    [[nodiscard]] uint32_t _allocateNodeCpu(const uint32_t t_idx) {
-        return 0;
+    [[nodiscard]] uint32_t _AllocateNewBlockCpu() {
+        CpuSpinLock lock(_spin_lock);
+        lock.lock();
+
+        uint32_t page_offset = _last_page_offset;
+        _last_page_offset += _thread_chunk_size_in_type;
+        if (_last_page_offset > kPageSizeInTypeSize) {
+            /* we exhausted current page -> we need new one */
+            /* performed in lazy manner */
+
+            ++_last_page;
+            assert(_last_page < kMaxPages);
+
+            _pages[_last_page] = new Node_[kPageSizeInTypeSize];
+
+            /* already offset for new chunk */
+            _last_page_offset = _thread_chunk_size_in_type;
+            page_offset = 0;
+        }
+
+        const uint32_t idx = _last_page * kPageSizeInTypeSize + page_offset;
+        lock.unlock();
+
+        return idx;
     }
 
     // ------------------------------
@@ -113,8 +262,8 @@ protected:
     // ------------------------------
 
     /* general data */
-    uint64_t _thread_chunk_size_in_type{};
-    uint64_t _num_threads{};
+    uint32_t _thread_chunk_size_in_type{};
+    uint32_t _num_threads{};
 
     /* thread data */
     uint32_t *_thread_tops{};
@@ -124,7 +273,7 @@ protected:
     Node_ **_pages{};
     uint32_t _last_page{};
     uint32_t _last_page_offset{};
+    uint32_t _spin_lock{};
 };
-
 
 #endif //ALLOCATORS_CUH
